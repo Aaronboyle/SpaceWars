f111cbc4ef2296c2e90917b49a3f653b0f9ff0fe
manjil21 <manjiltimilsina20@gmail.com>
2019-05-07 01:39:45 +1000

    Projectile object created
    this commit is about projectile object

--- SpaceWars/Assets/Scripts/PlayerController.cs
+++ SpaceWars/Assets/Scripts/PlayerController.cs
@@ -1,39 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[System.Serializable]
-/*
-public class Boundary
-{
-    public float xMin, xMax, zMin, zMax;
-}
- */
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Rigidbody rb;
     public float tilt;
-    public Done_Boundary boundary;
+    public Boundary boundary;
 
-
-
-   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        GetComponent<Rigidbody>().velocity = movement * speed;
-        /*
-        GetComponent<Rigidbody>().position = new Vector3
-        (
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-        );
+        rb.velocity = movement * speed;
+        Vector3 bounds = new Vector3((Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax)), 0.0f, (Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)));
 
-        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
-   */
+        rb.position = bounds;
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, (rb.velocity.x * -tilt));
     }
-
 }
+
+    [System.Serializable]
+    public class Boundary
+    {
+
+        public float xMin, xMax, zMin, zMax;
+
+    }﻿
+    
