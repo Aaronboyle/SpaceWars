34ccb253efa0e164bf008fdc5a779ea1e75220a4
Azat M <azatullah11@hotmail.com>
2019-05-11 21:04:56 +1000

    Scrolling background and starfield added
    Added a scrolling background, and also a moving starfield.

---
+++ Space_Shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPos;
+    }
+}
