01b5fd645c301d307ad89664e1fcf132188afe7f
manjil21 <manjiltimilsina20@gmail.com>
2019-05-18 20:34:45 +1000

    Main_menu
    Main menu with start and end button has been has been added

---
+++ Space_Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        if(explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+
+            if(!other.CompareTag("Player"))
+                Destroy(other.gameObject);
+        }
+            
+        if (other.CompareTag("Player"))
+        {
+            //Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            //gameController.GameOver();
+            gameController.PlayerHit();
+        }
+        else
+            gameController.AddScore(scoreValue);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Space_Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public float playerHealth;
+    public bool playerIsDead;
+
+    public float PlayerHealth
+    {
+        get => playerHealth;
+        set => playerHealth = value;
+    }
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Start()
+    {
+        playerHealth = 100.0f;
+        playerIsDead = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+
+        if (playerHealth < 0)
+            playerIsDead = true;
+    }
+
+
+    public float PlayerIsHit()
+    {
+        playerHealth -= 20;
+        return playerHealth;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3
+            (Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+            0.0f, 
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax));
+
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+}
