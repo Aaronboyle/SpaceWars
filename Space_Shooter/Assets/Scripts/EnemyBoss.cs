90264fa71b44683159daf4453c25bb8d85950cb7
azat22 <azatullah11@hotmail.com>
2019-05-27 13:37:39 +1000

    UNIT TEST: Added code that passes the unit test
    Added code to control the enemy boss health.

--- Space_Shooter/Assets/Scripts/EnemyBoss.cs
+++ Space_Shooter/Assets/Scripts/EnemyBoss.cs
@@ -16,13 +16,18 @@ public class EnemyBoss : MonoBehaviour
 
     public float TakeDamage()
     {
-        
+        if (bossHealth <= 0.0f)
+            bossIsDead = true;
+
+        bossHealth -= 10.0f;
+        return bossHealth;
     }
 
 
     public void Reset()
     {
-        
+        bossHealth = 100.0f;
+        bossIsDead = false;
     }
 
 
