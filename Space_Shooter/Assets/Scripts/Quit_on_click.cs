9a95c6615b9537d075ee059aab6599a3bf5fd75f
manjil21 <manjiltimilsina20@gmail.com>
2019-05-29 09:46:43 +1000

    Main Menu
    main menu had to be redone

---
+++ Space_Shooter/Assets/Scripts/Quit_on_click.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit_on_click : MonoBehaviour
+{
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
