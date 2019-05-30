2ba04b958ccc45e22eacdc5a8a0a99ca48d5e5ae
101113608 <101113608@student.swin.edu.au>
2019-05-30 20:24:05 +1000

    Added Unit Test #1
    - Added unit test for player regenerating their health

---
+++ Space_Shooter/Assets/_EditMode_Project_Tests/Editor/_UnitTest_Regen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	
+	public class _UnitTest_Regen
+    {
+	
+		//Test to see if player can regenerate health
+		[Test]
+        public void PlayerRegenTest() 
+		{
+        PlayerController player = new PlayerController();
+		
+		player.Start();
+		player.PlayerIsHit(); //Player takes 20 damage
+	//	player.PlayerRegenHealth(); //Regen 10 health
+	//	float health = player.PlayerCurrentHealth(); //Obtain health
+
+	//	Assert.AreEqual(90.0f, health);
+        }
+	
+    }
+	
+}
