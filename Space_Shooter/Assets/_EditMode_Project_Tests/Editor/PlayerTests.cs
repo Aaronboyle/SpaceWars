f78218767f1cf346334ff069bafae03e97a85665
Azat M <azatullah11@hotmail.com>
2019-05-30 17:11:20 +1000

    Changed method names in PlayerController and EnemyBoss
    Made the method names more descriptive

---
+++ Space_Shooter/Assets/_EditMode_Project_Tests/Editor/PlayerTests.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerTests
+    {
+        //Test to see that the player health decreases when the TakeDamage() method is called
+        //Player health starts at 100.0f, and decreases by 20.0f every hit
+        //[Test]
+        public void something()
+        {
+            GameController gameController = new GameController();
+            gameController.SpawnWaves();
+
+            Assert.AreEqual(2, gameController.wave);
+        }
+    }
+}
